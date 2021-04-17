5e82410e6beaa996ee3285befb52eea47f8bb1f8
Dhiraj Kumar <dhirajyadav18062000@gmail.com>
2021-04-17 21:42:58 +0530

    Sets app flow

--- app/src/main/java/dp/rater/dpraterhai/MainActivity.java
+++ app/src/main/java/dp/rater/dpraterhai/MainActivity.java
@@ -1,14 +1,37 @@
 package dp.rater.dpraterhai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        getSupportActionBar().hide();
+
+        BottomNavigationView bottom_nav = findViewById(R.id.bottom_nav);
+        // Passing each menu ID as a set of Ids because each menu is the top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.addFragment, R.id.profileFragment)
+                .build();
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottom_nav, navController);
+
+
     }
 }
