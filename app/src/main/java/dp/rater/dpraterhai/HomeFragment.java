bab7a0f0d98ff0c1ba5cb261bf13d7ea0a261490
18UCS051_Pramod Chaurasiya <pramodchaurasia51@gmail.com>
2021-04-17 20:04:57 +0530

    navigatio set kr do tum

--- app/src/main/java/dp/rater/dpraterhai/HomeFragment.java
+++ app/src/main/java/dp/rater/dpraterhai/HomeFragment.java
@@ -26,6 +26,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.home_item, container, false);
     }
 }
